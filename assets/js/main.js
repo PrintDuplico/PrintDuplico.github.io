0cb3cd78bed89a184224026aace2ab68a9a29f52
explne <ahmed@mufti.dev>
2021-04-26 01:03:48 +0200

    Initial Commit

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+// Change iframe-wrapper class on click
+
+function changeClass() {
+    const el = document.querySelector(".iframe-wrapper");
+    el.classList.remove(".iframe-wrapper_height_inital");
+    el.classList.add(".iframe-wrapper_height_expand");
+}
+
+function clickListener() {
+    document.addEventListener("click", () => {
+        changeClass();
+    });
+}
+
+clickListener();
